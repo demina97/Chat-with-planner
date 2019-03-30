f3841c03d8fed03873eddd2707f0f7491a6d3d27
demina97 <natashademina23@gmail.com>
2019-03-30 13:13:48 +0600

    first commit

---
+++ src/main/java/kz/aupet/vt152/diplom/DiplomApplication.java
@@ -0,0 +1,14 @@
+package kz.aupet.vt152.diplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/kz/aupet/vt152/diplom/DiplomApplicationTests.java
@@ -0,0 +1,17 @@
+package kz.aupet.vt152.diplom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiplomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
