205795ed0b2616b9cb1848f8be751aa414494e3d
demina97 <natashademina23@gmail.com>
2019-03-30 15:37:49 +0600

    Стили для хидера, перенос чата в отдельный компонент

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import {FormsModule} from "@angular/forms";
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/chat/chat.component.ts
@@ -0,0 +1,41 @@
+import { Component} from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent {
+
+  private serverUrl = 'http://localhost:8080/socket';
+  private title = 'WebSockets chat';
+  private stompClient;
+  public messages: string[] = [];
+  public input: string = "";
+
+  constructor(){
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/chat", (message) => {
+        if(message.body) {
+          that.messages.push(message.body);
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(){
+    this.stompClient.send("/app/send/message" , {}, this.input);
+    this.input = "";
+  }
+
+}
