681a75da3d77469b252256c1c4a425589a342396
demina97 <natashademina23@gmail.com>
2019-04-06 18:50:19 +0600

    исправлена отправка сообщения

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component } from '@angular/core';
-import * as Stomp from 'stompjs';
-import * as SockJS from 'sockjs-client';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,33 +6,7 @@ import * as SockJS from 'sockjs-client';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private serverUrl = 'http://localhost:8080/socket';
-  private title = 'WebSockets chat';
-  private stompClient;
-  public messages: string[] = [];
-  public input: string = "";
-
   constructor(){
-    this.initializeWebSocketConnection();
-  }
-
-  initializeWebSocketConnection(){
-    let ws = new SockJS(this.serverUrl);
-    this.stompClient = Stomp.over(ws);
-    let that = this;
-    this.stompClient.connect({}, function(frame) {
-      that.stompClient.subscribe("/chat", (message) => {
-        if(message.body) {
-          that.messages.push(message.body);
-          console.log(message.body);
-        }
-      });
-    });
-  }
-
-  sendMessage(){
-    this.stompClient.send("/app/send/message" , {}, this.input);
-    this.input = "";
   }
 
 }
